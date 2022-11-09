c56816e705b34ac2d68424018c460bd8200179fd
Piyushnmj <piyushnmj@yahoo.com>
2022-11-09 20:24:25 +0530

    Welcome

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Problem");
+        }
+    }
+}
