5d2d4672d58ab4efbd37c9ef8dbad90d2dbbdbb7
Piyushnmj <piyushnmj@yahoo.com>
2022-11-09 20:53:52 +0530

    Adding numbers to Stack

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue Problem");
+
+            LinkedList objList = new LinkedList(); 
+            objList.Push(56);
+            objList.Push(30);
+            objList.Push(70);
+            objList.Display();
         }
     }
 }
